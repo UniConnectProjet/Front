b9a2bbf413dae9914b7d77fefc4e4c182107310a
inesb <bourhim.ines@gmail.com>
2025-05-10 00:53:29 +0200

    Component Absences : done

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,5 +2,6 @@ import Form from "./Form/Form";
 import LogoFond from "./LogoFond/LogoFond";
 import Menu from "./Menu/Menu";
 import GradeCard from "./GradeCard/GradeCard";
+import Absence from "./Absence/Absence";
 
-export { Form, LogoFond, Menu, GradeCard };
+export { Form, LogoFond, Menu, GradeCard, Absence };
