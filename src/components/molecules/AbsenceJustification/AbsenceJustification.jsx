fe4a6ac8a434aa66df5ca5f65c5a49f2e8224be3
inesb <bourhim.ines@gmail.com>
2025-06-25 14:43:52 +0200

    update ci/cd

--- src/components/molecules/AbsenceJustification/AbsenceJustification.jsx
+++ src/components/molecules/AbsenceJustification/AbsenceJustification.jsx
@@ -6,14 +6,14 @@ import exclamation from "../../../assets/svg/exclamation.svg";
 const AbsenceJustification = ({ justification, title, date, hours, className = "" }) => {
     return (
         <div className={`flex p-2 mr-4 justify-between ${className}`}>
-            <div className="flex p-2">
+            <div className="flex p-2 items-center">
                 <Image src={exclamation} alt="UnjustifiedAbsence" className="w-8 h-8 rounded-full" />
                 <div className="flex flex-col justify-between p-2">
                     <Title className="text-sm font-bold text-gray-800">{date}</Title>
                     <Text>{hours}</Text>
                 </div>
             </div>
-            <div className="p-2">
+            <div className="flex p-2 justify-center items-center">
                 <Button onClick={justification} className="px-3 py-1 border-primary-500 bg-gray-200 rounded-md text-primary-500">
                     {title}
                 </Button>
